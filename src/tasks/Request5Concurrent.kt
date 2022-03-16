e9a1c77fd5db4e77efc29012367ab26d64f1267c
Vitor Brandão <vitor.brandao@ifood.com.br>
2022-03-16 10:43:52 -0300

    load contributors concurrently using suspend functions

--- src/tasks/Request5Concurrent.kt
+++ src/tasks/Request5Concurrent.kt
@@ -4,5 +4,20 @@ import contributors.*
 import kotlinx.coroutines.*
 
 suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> = coroutineScope {
-    TODO()
+
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: listOf()
+
+    val contributorsDeferred = repos.map { repo ->
+        async {
+            service
+                .getRepoContributors(req.org, repo.name)
+                .also { logUsers(repo, it) }
+                .bodyList()
+        }
+    }
+
+    contributorsDeferred.awaitAll().flatten().aggregate()
 }
