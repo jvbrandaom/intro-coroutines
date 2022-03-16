fb7b5f3d40cd1467dc085c443c0f366daaa79f04
Vitor Brandão <vitor.brandao@ifood.com.br>
2022-03-16 11:21:52 -0300

    load contributors showing progress without concurrency

--- src/tasks/Request6Progress.kt
+++ src/tasks/Request6Progress.kt
@@ -7,5 +7,22 @@ suspend fun loadContributorsProgress(
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: listOf()
+
+    var allUsers = mutableListOf<User>()
+
+    repos.forEach { repo ->
+        allUsers.addAll(service
+            .getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList())
+
+        allUsers = allUsers.aggregate().toMutableList()
+        updateResults(allUsers, false)
+    }
+
+    updateResults(allUsers, true)
 }
