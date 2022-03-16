9b1103d26a94cb3ec6d59931a23fc302fe108ae5
Vitor Brandão <vitor.brandao@ifood.com.br>
2022-03-16 11:59:22 -0300

    load contributors with channels

--- src/tasks/Request7Channels.kt
+++ src/tasks/Request7Channels.kt
@@ -11,6 +11,29 @@ suspend fun loadContributorsChannels(
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
     coroutineScope {
-        TODO()
+        val channel = Channel<List<User>>()
+        val repos = service
+            .getOrgRepos(req.org)
+            .also { logRepos(req, it) }
+            .body() ?: listOf()
+        var allUsers = listOf<User>()
+
+        for (repo in repos) {
+            log("starting loading for ${repo.name}")
+            launch {
+                val users = service
+                    .getRepoContributors(req.org, repo.name)
+                    .also { logUsers(repo, it) }
+                    .bodyList()
+
+                channel.send(users)
+            }
+        }
+
+        repeat(repos.size) {
+            val users = channel.receive()
+            allUsers = (allUsers + users).aggregate()
+            updateResults(allUsers, it == repos.lastIndex)
+        }
     }
 }
